98ef3f789492e4dfd1648b39177b1a3e8639b8a2
Yeremi Tantaraico <yeremitantaraico@gmail.com>
2025-12-30 12:26:01 -0500

    Fix for using local CDNs

--- inc/scripts.php
+++ inc/scripts.php
@@ -1,5 +1,5 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="scripts/jquery-1.12.4.min.js"></script>
+      <script src="scripts/bootstrap-3.3.7.min.js"></script>
       <script>
          $(".nav .dropdown").hover(function() {
            $(this).find(".dropdown-toggle").dropdown("toggle");
