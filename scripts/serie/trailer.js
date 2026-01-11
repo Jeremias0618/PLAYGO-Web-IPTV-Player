2c2b65aec534a6f8e9ba15a62436838e70ed65fb
Yeremi Tantaraico <yeremitantaraico@gmail.com>
2026-01-11 12:55:51 -0500

    Trailer playback correction: video and audio pause when closing the modal in movies and series

--- scripts/serie/trailer.js
+++ scripts/serie/trailer.js
@@ -17,32 +17,60 @@
         }
 
         const trailerModal = new bootstrap.Modal(trailerModalElement);
+        let currentYoutubeUrl = '';
+        
+        function stopTrailer() {
+            if (trailerIframe) {
+                const currentSrc = trailerIframe.src;
+                
+                if (currentSrc && currentSrc.includes('youtube.com')) {
+                    try {
+                        trailerIframe.contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
+                    } catch (e) {
+                    }
+                    
+                    setTimeout(function() {
+                        try {
+                            const url = new URL(currentSrc);
+                            const videoId = url.pathname.split('/').pop().split('?')[0];
+                            if (videoId) {
+                                trailerIframe.src = "https://www.youtube.com/embed/" + videoId + "?autoplay=0&enablejsapi=1";
+                            }
+                        } catch (e) {
+                        }
+                        
+                        setTimeout(function() {
+                            trailerIframe.src = "about:blank";
+                        }, 200);
+                    }, 100);
+                } else {
+                    trailerIframe.src = "about:blank";
+                }
+            }
+        }
         
         const closeBtn = trailerModalElement.querySelector('.btn-close');
         if (closeBtn) {
             closeBtn.addEventListener('click', function(e) {
                 e.preventDefault();
                 stopTrailer();
-                trailerModal.hide();
+                setTimeout(function() {
+                    trailerModal.hide();
+                }, 50);
             });
         }
 
-        function stopTrailer() {
-            if (trailerIframe) {
-                trailerIframe.src = "about:blank";
-            }
-        }
-
         btnTrailer.addEventListener('click', function() {
-            trailerIframe.src = "https://www.youtube.com/embed/" + youtubeId + "?autoplay=1";
+            currentYoutubeUrl = "https://www.youtube.com/embed/" + youtubeId + "?autoplay=1&enablejsapi=1";
+            trailerIframe.src = currentYoutubeUrl;
             trailerModal.show();
         });
 
-        trailerModalElement.addEventListener('hide.bs.modal', function () {
+        trailerModalElement.addEventListener('hide.bs.modal', function (e) {
             stopTrailer();
         });
 
-        trailerModalElement.addEventListener('hidden.bs.modal', function () {
+        trailerModalElement.addEventListener('hidden.bs.modal', function (e) {
             stopTrailer();
         });
         
